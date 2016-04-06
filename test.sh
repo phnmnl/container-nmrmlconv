73169016d31401cd2191cd63b484f46c496cda52
Daniel Jacob <djacob65@gmail.com>
2016-04-06 09:07:52 +0200

    update

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -i -v `pwd`/examples:/data --rm nmrmlconv \
-      -l create -b -z -t bruker -i /data/MMBBI_10M12-CE01-1a/1 -o /data/MMBBI_10M12-CE01-1a.nmrML
-
-docker run -i -v `pwd`/examples:/data --rm nmrmlconv \
-      -l proc -i /data/MMBBI_10M12-CE01-1a.nmrML -d /data/MMBBI_10M12-CE01-1a/1/pdata/1 -o /data/MMBBI_10M12-CE01-1a.nmrML
-
-
