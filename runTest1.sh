e99256572fa1ffd41b2c7bd83c23347663124726
Kristian Peters <kristian@korseby.net>
2017-05-31 15:35:42 +0200

    updates testing with test-file

--- runTest1.sh
+++ runTest1.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-venformat="$1"
-zipfile="$2"
-infile="$3"			#"BPA_c21_aq_126-BPA25ng/1"
-outfile="$4"
-
 cd /data
+
+# Fetch RAW data
 wget -O test.zip 'https://github.com/phnmnl/container-nmrmlconv/raw/develop/BPA_c21_aq_126-BPA25ng.zip'
 unzip -q -d /data/test test.zip
 
+# Conversion
 java -jar /usr/local/share/nmrML/converter.jar -l create -b -z -t Bruker -i /data/test/ -o /data/test.nmrML
 
+# Fetch expected output
+wget -O expected.nmrML 'https://raw.githubusercontent.com/phnmnl/container-nmrmlconv/develop/BPA_c21_aq_126-BPA25ng.nmrML'
+
+# Compare
+cmp --silent expected.nmrML test.nmrML
+
+if [[ $? -eq 0 ]]; then
+	echo "Files are identical."
+else
+	echo "Error! Files are not identical."
+	exit 2
+fi
 
